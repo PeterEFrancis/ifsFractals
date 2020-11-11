fb898631465e03399b1a0cf89a386027693386fd
Peter E. Francis <franpe02@gettysburg.edu>
2020-11-11 11:45:28 -0500

    change database structure and add post link in

--- app.py
+++ app.py
@@ -30,12 +30,23 @@ class Playground(db.Model):
     __tablename__ = "playgrounds"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
-    info = db.Column(db.Text)
+    transformations = db.Column(db.Text)
+    weights = db.Column(db.Text)
+    vars = db.Column(db.Text)
+    zoom = db.Column(db.Text)
+    center = db.Column(db.Text)
+    points = db.Column(db.Text)
+    color = db.Column(db.Text)
 
-    def __init__(self, name, info):
+    def __init__(self, name, transformations, weights, vars, zoom, center, points, color):
         self.name = name
-        self.info = info
-
+        self.transformations = transformations
+        self.weights = weights
+        self.vars = vars
+        self.zoom = zoom
+        self.center = center
+        self.points = points
+        self.color = color
 
 
 def is_a_playground_name(name):
@@ -47,14 +58,34 @@ def get_new_name():
 
 
 
+def save(transformations, weights, vars, zoom, center, points, color):
+    name = get_new_name()
+    playground = Playground(
+        name = name,
+        transformations = transformations,
+        weights = weights,
+        vars = vars,
+        zoom = zoom,
+        center = center,
+        points = points,
+        color = color
+    )
+    db.session.add(playground)
+    db.session.commit()
+    return name
+
 
 @app.route('/save_playground', methods=['POST'])
 def save_playground():
     if request.method == 'POST':
-        name = get_new_name()
-        playground = Playground(name = name, info = request.form['info'])
-        db.session.add(playground)
-        db.session.commit()
+        transformations = request.form['transformations']
+        weights = request.form['weights']
+        vars = request.form['vars']
+        zoom = request.form['zoom']
+        center = request.form['center']
+        points = request.form['points']
+        color = request.form['color']
+        name = save(transformations, weights, vars, zoom, center, points, color)
         return jsonify({'success':'true', 'name':name})
     return 'Access Denied'
 
@@ -79,14 +110,14 @@ examples = ['barnsley', 'serpinski', 'vonkoch', 'dragon', 'crab', 'tree']
 @app.route('/playground')
 @app.route('/playground/')
 def playground():
-    return redirect('/playground/' + random.choice(examples))
+    return redirect('/playground/barnsley') # + random.choice(examples))
 
 
 @app.route('/playground/<string:s>')
 def playground_saved(s):
     if is_a_playground_name(s):
-        info = db.session.query(Playground).filter(Playground.name == s)[0].info
-        return render_template('playground.html', info=info)
+        playground = db.session.query(Playground).filter(Playground.name == s)[0]
+        return render_template('playground.html', playground=playground)
     else:
         return "no playground found"
 
@@ -102,6 +133,16 @@ def master():
 
 
 
+@app.route('/playground/t=<string:transformations>/w=<string:weights>')
+def m(transformations, weights):
+    name = save(transformations, weights, "{}", "auto", '{"x":0,"y":0}', "10000", "#000000")
+    return redirect('/playground/' + name)
+
+
+
+
+
+
 
 
  #  _       _ _   _       _ _
@@ -111,16 +152,28 @@ def master():
  # |_|_| |_|_|\__|_|\__,_|_|_/___\___|
 
 
-@app.route('/initialize')
+# @app.route('/initialize')
 def initialize():
+    db.drop_all()
     db.create_all()
     directory = 'static/examples/'
     for filename in os.listdir(directory):
         with open(os.path.join(directory, filename)) as f:
-            playground = Playground(name=filename, info=f.read().strip())
+            info = eval(f.read().replace('\n','').replace(' ',''))
+            playground = Playground(
+                name = filename,
+                transformations = info['transformations'],
+                weights = info['weights'],
+                vars = info['vars'],
+                zoom = info['zoom'],
+                center = info['center'],
+                points = info['points'],
+                color = info['color']
+            )
+            print(playground.transformations)
             db.session.add(playground)
             db.session.commit()
-
+    return 'done'
 
 
  #   __ _ _ __  _ __    _ __ _   _ _ __
